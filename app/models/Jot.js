c8dd5fc3dcd5c32c32ba285ea9204dc82b1d61f8
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2023-10-06 16:35:11 -0600

    Mobile Support Change

--- app/models/Jot.js
+++ app/models/Jot.js
@@ -29,31 +29,29 @@ export class Jot {
     get activeCardTemplate() {
         return /*html*/`
         <div class="col-9 text-center p-2 jot-card">
-          <!-- Active Jot Header -->
-          <div class="d-flex justify-content-between p-3">
-          <p class="fs-3"> ${this.title}<i class="mdi mdi-star-circle-outline color-icon" style="color: ${this.color};"></i></p>
-            <div class="d-flex">
-                <button onclick="app.JotsController.openJot(-1)" class="btn btn-secondary rounded-5 mx-2"><i class="mdi mdi-arrow-left"></i></button>
-                <button onclick="app.JotsController.deleteJot('${this.id}')" class="btn btn-danger rounded-5"><i class="mdi mdi-trash-can"></i></button>
+            <!-- Active Jot Header -->
+            <div class="d-flex justify-content-between p-3">
+                <p class="fs-3"> ${this.title}<i class="mdi mdi-star-circle-outline color-icon" style="color: ${this.color};"></i></p>
+                <div class="d-flex">
+                    <button onclick="app.JotsController.openJot(-1)" class="btn btn-secondary rounded-5 mx-2"><i class="mdi mdi-arrow-left"></i></button>
+                    <button onclick="app.JotsController.deleteJot('${this.id}')" class="btn btn-danger rounded-5"><i class="mdi mdi-trash-can"></i></button>
                 </div>
-                </div>
+            </div>
                 
-                <!-- Jot Body -->
-                <div class="row p-3">
+            <!-- Jot Body -->
+            <div class="row p-3 ">
                 <!-- Jot Data -->
-                <div class="col-5 text-grey text-start">
-                <p class="mb-3">Created: ${this.createdDate.toLocaleString()}</p>
-                <p class="mb-3">Edited: ${_readableTime((new Date() - this.editedDate))} Ago</p>
-                <p> Words: ${this.wordCount}, Characters: ${this.charCount}</p>
+                <div class="col-12 col-md-5 text-grey text-start">
+                    <p class="mb-3">Created: ${this.createdDate.toLocaleString()}</p>
+                    <p class="mb-3">Edited: ${_readableTime((new Date() - this.editedDate))} Ago</p>
+                    <p> Words: ${this.wordCount}, Characters: ${this.charCount}</p>
                 </div>
                 <!-- Jot Writing Area -->
-                <div class="col-7 text-start">
-                <textarea onblur="app.JotsController.saveActiveJot()" name="content" id="activeJotContent" class="rounded p-2 w-100" rows="14"
-                placeholder="Jot Down Your Thoughts...">${this.content}</textarea>
+                <div class="col-12 col-md-7 text-start">
+                    <textarea onblur="app.JotsController.saveActiveJot()" name="content" id="activeJotContent" class="rounded p-2 w-100" rows="14"
+                        placeholder="Jot Down Your Thoughts...">${this.content}</textarea>
                 </div>
-                </div>
-    
-
+            </div>
         </div>
         `
     }
